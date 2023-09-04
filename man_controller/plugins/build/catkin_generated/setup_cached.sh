190bd0b9fb8632a3f076d03365cd865c5f15d376
Stav42 <vaditya@iitk.ac.in>
2023-09-04 13:02:08 +0530

    idontknowhatishappening

--- man_controller/plugins/build/catkin_generated/setup_cached.sh
+++ man_controller/plugins/build/catkin_generated/setup_cached.sh
@@ -10,4 +10,4 @@
 export CMAKE_PREFIX_PATH="/Users/stav.42/ws/src/man_controller/plugins/build/devel:$CMAKE_PREFIX_PATH"
 export PKG_CONFIG_PATH='/Users/stav.42/ws/devel/lib/pkgconfig:/Users/stav.42/miniconda3/envs/ros/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/lib/pkgconfig/'
 export PYTHONPATH='/Users/stav.42/ws/devel/lib/python3.9/site-packages:/Users/stav.42/miniconda3/envs/ros/lib/python3.9/site-packages:/Users/stav.42/Dev/rbdl/build/python:/usr/local/lib/python3.9/site-packages'
-export ROS_PACKAGE_PATH='/Users/stav.42/ws/src/man_controller/plugins:/Users/stav.42/ws/src/man_controller:/Users/stav.42/ws/src/man_description:/Users/stav.42/ws/src/PlotJuggler:/Users/stav.42/ws/src/plotjuggler_msgs:/Users/stav.42/ws/src/plotjuggler-ros-plugins:/Users/stav.42/miniconda3/envs/ros/share'
+export ROS_PACKAGE_PATH="/Users/stav.42/ws/src/man_controller/plugins:$ROS_PACKAGE_PATH"
